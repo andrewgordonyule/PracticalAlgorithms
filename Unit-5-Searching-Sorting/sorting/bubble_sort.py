2dbd857ce44010aa168c544f1a7b24c50b428280
andrewgordonyule <andrewgordonyule@gmail.com>
2022-01-07 22:40:45 +0000

    add insertion sort algorithm

--- Unit-5-Searching-Sorting/sorting/bubble_sort.py
+++ Unit-5-Searching-Sorting/sorting/bubble_sort.py
@@ -2,7 +2,9 @@
 
 # Time Complexity: O(n^2)
 def bubblesort(arr):
+
     n = len(arr)
+
     for outer in range(n - 1, 0, -1):
         for i in range(outer):
             if arr[i] > arr[i + 1]:
@@ -11,9 +13,6 @@ def bubblesort(arr):
 
 if __name__ == "__main__":
     myNumbers = [5, 3, 7, 1, 4, 2, 9, 6, 8, 0]
-
     print("Unsorted:\t", myNumbers)
-
     bubblesort(myNumbers)
-
     print("Sorted:\t\t", myNumbers)
---
+++ Unit-5-Searching-Sorting/sorting/insertion_sort.py
@@ -0,0 +1,23 @@
+# Insertion Sort Algorithm
+
+# Time Complexity: O(n^2)
+def insertion_sort(arr):
+
+    n = len(arr)
+
+    for outer in range(1, n):
+        key = arr[outer]
+        inner = outer
+
+        while inner > 0 and arr[inner - 1] > key:
+            arr[inner] = arr[inner - 1]
+            inner -= 1
+
+        arr[inner] = key
+
+
+if __name__ == "__main__":
+    myNumbers = [5, 3, 7, 1, 4, 2, 9, 6, 8, 0]
+    print("Unsorted:\t", myNumbers)
+    insertion_sort(myNumbers)
+    print("Sorted:\t\t", myNumbers)
